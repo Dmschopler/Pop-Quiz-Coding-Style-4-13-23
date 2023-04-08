7bb98f4680317ffd3f7652fce43ae2090b7a3385
Daniel S <dmschopler@gmail.com>
2023-04-08 12:38:49 -0400

    Updated html and javaScript

--- develop/quizcode.js
+++ develop/quizcode.js
@@ -1,3 +1,29 @@
-var hello = "hello world"
+var secondsLeft = 10;
+var timeEl = document.querySelector(".time");
+var start = document.querySelector(".start-button")
 
-console.log(hello)
+function showQuestion(event) {
+    
+}
+
+start.addEventListener("click", function() {
+    if ()
+})
+
+function setTime() {
+    var timerTotal  = setInterval(function() {
+        secondsLeft--;
+        timeEl.textContent = "Time Left: " + secondsLeft;
+        
+        if (secondsLeft === 0) {
+            clearInterval(timerTotal);
+            sendMessage();
+        }
+    }, 1000);
+}
+
+function sendMessage() {
+    timeEl.textContent = "Time's Up!";
+}
+
+setTime();
